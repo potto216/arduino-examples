aff31131f02467734e61536f986e37475600b84e
Paul Otto <potto@ieee.org>
2023-01-22 09:14:23 -0500

    updated command structure and how the led acts

--- bluetooth/ble_gatt_server_simulator/ble_gatt_server_simulator.ino
+++ bluetooth/ble_gatt_server_simulator/ble_gatt_server_simulator.ino
@@ -24,21 +24,34 @@ b3f8665e-9514-11ed-9f96-37eb16895c5e
 BLEService Ramp01ServiceByte("b3f8665e-9514-11ed-9f96-37eb16895c01");
 BLEService Ramp02ServiceByte("b3f8665e-9514-11ed-9f96-37eb16895c02");
 
+static constexpr byte RAMP_COMMAND_STOP = 0;
+static constexpr byte RAMP_COMMAND_START = 1;
+static constexpr byte RAMP_COMMAND_RESET = 2;
+
+static constexpr byte RAMP_COMMAND_RESULT_SUCCESS = 0;
+static constexpr byte RAMP_COMMAND_RESULT_NONE_RECEIVED = 1;
+static constexpr byte RAMP_COMMAND_RESULT_ERROR = 2;
+
+static constexpr byte RAMP_STATUS_STOPPED = 0;
+static constexpr byte RAMP_STATUS_RUNNING = 1;
+
 //
 BLEByteCharacteristic Ramp01MinimumValue("b5720d32-9514-11ed-985d-7300cdba6b00", BLERead | BLEWrite);
 BLEByteCharacteristic Ramp01MaximumValue("b5720d32-9514-11ed-985d-7300cdba6b01", BLERead | BLEWrite);
 BLEByteCharacteristic Ramp01CurrentValue("b5720d32-9514-11ed-985d-7300cdba6b02", BLERead );
-BLEByteCharacteristic Ramp01Start("b5720d32-9514-11ed-985d-7300cdba6b03", BLEWrite);
-BLEByteCharacteristic Ramp01Stop("b5720d32-9514-11ed-985d-7300cdba6b04", BLEWrite);
-BLEByteCharacteristic Ramp01Reset("b5720d32-9514-11ed-985d-7300cdba6b05", BLEWrite);
+BLEByteCharacteristic Ramp01Command("b5720d32-9514-11ed-985d-7300cdba6b03", BLEWrite);
+BLEByteCharacteristic Ramp01CommandStatus("b5720d32-9514-11ed-985d-7300cdba6b04", BLERead | BLENotify );
+BLEByteCharacteristic Ramp01Status("b5720d32-9514-11ed-985d-7300cdba6b05", BLERead | BLENotify );
+BLEByteCharacteristic Ramp01StepTime("b5720d32-9514-11ed-985d-7300cdba6b06", BLERead | BLEWrite );
+
 
 BLEByteCharacteristic Ramp02MinimumValue("b5720d32-9514-11ed-985d-7300cdba6b00", BLERead | BLEWrite);
 BLEByteCharacteristic Ramp02MaximumValue("b5720d32-9514-11ed-985d-7300cdba6b01", BLERead | BLEWrite);
 BLEByteCharacteristic Ramp02CurrentValue("b5720d32-9514-11ed-985d-7300cdba6b02", BLERead );
-BLEByteCharacteristic Ramp02Start("b5720d32-9514-11ed-985d-7300cdba6b03", BLEWrite);
-BLEByteCharacteristic Ramp02Stop("b5720d32-9514-11ed-985d-7300cdba6b04", BLEWrite);
-BLEByteCharacteristic Ramp02Reset("b5720d32-9514-11ed-985d-7300cdba6b05", BLEWrite);
-
+BLEByteCharacteristic Ramp02Command("b5720d32-9514-11ed-985d-7300cdba6b03", BLEWrite);
+BLEByteCharacteristic Ramp02CommandStatus("b5720d32-9514-11ed-985d-7300cdba6b04", BLERead | BLENotify );
+BLEByteCharacteristic Ramp02Status("b5720d32-9514-11ed-985d-7300cdba6b05", BLERead | BLENotify );
+BLEByteCharacteristic Ramp02StepTime("b5720d32-9514-11ed-985d-7300cdba6b06", BLERead | BLEWrite );
 
 
 void setup() {
@@ -57,19 +70,20 @@ BLE.setLocalName("GATT_Server");
 Ramp01ServiceByte.addCharacteristic(Ramp01MinimumValue);
 Ramp01ServiceByte.addCharacteristic(Ramp01MaximumValue);
 Ramp01ServiceByte.addCharacteristic(Ramp01CurrentValue);
-Ramp01ServiceByte.addCharacteristic(Ramp01Start);
-Ramp01ServiceByte.addCharacteristic(Ramp01Stop);
-Ramp01ServiceByte.addCharacteristic(Ramp01Reset);
+Ramp01ServiceByte.addCharacteristic(Ramp01Command);
+Ramp01ServiceByte.addCharacteristic(Ramp01CommandStatus);
+Ramp01ServiceByte.addCharacteristic(Ramp01Status);
+Ramp01ServiceByte.addCharacteristic(Ramp01StepTime);
 BLE.addService(Ramp01ServiceByte);
 
 Ramp02ServiceByte.addCharacteristic(Ramp02MinimumValue);
 Ramp02ServiceByte.addCharacteristic(Ramp02MaximumValue);
 Ramp02ServiceByte.addCharacteristic(Ramp02CurrentValue);
-Ramp02ServiceByte.addCharacteristic(Ramp02Start);
-Ramp02ServiceByte.addCharacteristic(Ramp02Stop);
-Ramp02ServiceByte.addCharacteristic(Ramp02Reset);
+Ramp01ServiceByte.addCharacteristic(Ramp02Command);
+Ramp01ServiceByte.addCharacteristic(Ramp02CommandStatus);
+Ramp01ServiceByte.addCharacteristic(Ramp02Status);
+Ramp01ServiceByte.addCharacteristic(Ramp02StepTime);
 BLE.addService(Ramp02ServiceByte);
-
 
 BLE.setAdvertisedService(Ramp01ServiceByte);
 // 1600*0.625 msec = 1 sec
@@ -87,19 +101,83 @@ if (central)
 {
 Serial.print("Connected to central: ");
 Serial.println(central.address());
-digitalWrite(LED_BUILTIN, HIGH);
+
+Ramp01CommandStatus.writeValue(RAMP_COMMAND_RESULT_NONE_RECEIVED);
 Ramp01MinimumValue.writeValue(1);
 Ramp01MaximumValue.writeValue(253);
 Ramp01CurrentValue.writeValue(Ramp01MinimumValue.value());
+Ramp01Status.writeValue(RAMP_STATUS_STOPPED);
+Ramp01StepTime.writeValue(1);
+
+
+const byte RAMP_COMMAND_RESULT_SUCCESS = 0;
+const byte RAMP_COMMAND_RESULT_NONE_RECEIVED = 1;
+const byte RAMP_COMMAND_RESULT_ERROR = 2;
+
+const byte RAMP_STATUS_STOPPED = 0;
+const byte RAMP_STATUS_RUNNING = 1;
+
+PinStatus ledState=HIGH;
+digitalWrite(LED_BUILTIN, ledState);
 while (central.connected()) {
      
-      Ramp01CurrentValue.writeValue(Ramp01CurrentValue.value()+1);
-      Serial.print("Ramp01 value is : ");
-      Serial.print(Ramp01CurrentValue.value());
-      delay(1000);
-}
+      if (Ramp01Command.written())
+      {
+        byte command = Ramp01Command.value();
+        switch(command)
+        {
+          case RAMP_COMMAND_STOP:
+          Serial.println("RAMP_COMMAND_STOP received.");
+            Ramp01Status.writeValue(RAMP_STATUS_STOPPED);
+            Ramp01CommandStatus.writeValue(RAMP_COMMAND_RESULT_SUCCESS);
+            break;
+
+          case RAMP_COMMAND_START:
+          Serial.println("RAMP_COMMAND_START received.");
+            Ramp01Status.writeValue(RAMP_STATUS_RUNNING);
+            Ramp01CommandStatus.writeValue(RAMP_COMMAND_RESULT_SUCCESS);
+            break;
+
+          case RAMP_COMMAND_RESET:
+            Serial.println("RAMP_COMMAND_RESET received.");
+            Ramp01MinimumValue.writeValue(1);
+            Ramp01MaximumValue.writeValue(253);
+            Ramp01CurrentValue.writeValue(Ramp01MinimumValue.value());
+            Ramp01Status.writeValue(RAMP_STATUS_STOPPED);
+            Ramp01StepTime.writeValue(1);
+            Ramp01CommandStatus.writeValue(RAMP_COMMAND_RESULT_SUCCESS);
+            break;
+
+          default:
+            Serial.print("Unsupported command received of 0x");
+            Serial.println(command, HEX);
+            Ramp01CommandStatus.writeValue(RAMP_COMMAND_RESULT_ERROR);
+
+        }
+
+      }
+
+
+      if (Ramp01Status.value() == RAMP_STATUS_RUNNING )
+      {
+        Serial.print("Ramp01 value is ");
+        Serial.println(Ramp01CurrentValue.value());
+        delay(Ramp01StepTime.value()*1000); 
+        if(Ramp01CurrentValue.value()>=Ramp01MaximumValue.value())
+        {
+          Ramp01CurrentValue.writeValue(Ramp01MinimumValue.value());
+        }
+        else
+        {
+          Ramp01CurrentValue.writeValue(Ramp01CurrentValue.value()+1);
+        } 
+        ledState= (ledState==HIGH) ? LOW : HIGH;       
+        digitalWrite(LED_BUILTIN, ledState);
+
+      }
+  }
 }
 digitalWrite(LED_BUILTIN, LOW);
-Serial.print("Disconnected from central: ");
-Serial.println(central.address());
+//Serial.print("Disconnected from central: ");
+//Serial.println(central.address());
 }
